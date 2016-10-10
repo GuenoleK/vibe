522cfc8b8ea3538eb56bd05a4dc00e65620c285e
GuenoleK <kikabouguenole@gmail.com>
2016-10-10 23:40:34 +0200

    Doing the example to get a data from a fetch

--- src/app/services/test_service.js
+++ src/app/services/test_service.js
@@ -3,7 +3,9 @@ import fetch from 'isomorphic-fetch';
 
 async function serverCaller() {
   const response = await fetch(`http://localhost:9999/api/helloworld`);
-  console.log(response.statusText);
+  response.text().then((data) => {
+    console.log(data);
+  })
 }
 
 export default serverCaller;
