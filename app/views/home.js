6b07d822138430a5a59143b61a07e9acce1b789d
GuenoleK <kikabouguenole@gmail.com>
2017-03-09 18:16:43 +0100

    Updatin the view

--- app/views/home.js
+++ app/views/home.js
@@ -6,97 +6,97 @@ import {Link} from 'react-router';
 
 export class HomeView extends Component {
 
-  render() {
-    return (
-      <div>
-      <RaisedButton label='server action' secondary={true} style={{margin: '30px'}} onClick={() => serverCaller()} />
-        <RaisedButton label='Test edit' secondary={true} style={{margin: '30px'}} onClick={() => this.props.router.push('edit-article/1')}/>
-        <div id='cards-container'>
-          <div data-vibe='card'>
-            <Card>
-              <CardMedia
-                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
-              </CardMedia>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
-                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-              </CardText>
-              <CardActions>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-              </CardActions>
-            </Card>
-          </div>
+    render() {
+        return (
+            <div className='home-container'>
+                <RaisedButton label='server action' secondary={true} style={{margin: '30px'}} onClick={() => serverCaller()} />
+                <RaisedButton label='Test edit' secondary={true} style={{margin: '30px'}} onClick={() => this.props.router.push('edit-article/1')}/>
+                <div id='home-cards-container'>
+                    <div data-vibe='card'>
+                        <Card>
+                            <CardMedia
+                                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                                >
+                                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
+                            </CardMedia>
+                            <CardTitle title="Card title" subtitle="Card subtitle" />
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
+                                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
+                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                            </CardText>
+                            <CardActions>
+                                <FlatButton label="Action1" />
+                                <FlatButton label="Action2" />
+                            </CardActions>
+                        </Card>
+                    </div>
 
-          <div data-vibe='card'>
-            <Card>
-              <CardMedia
-                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
-              </CardMedia>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
-                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-              </CardText>
-              <CardActions>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-              </CardActions>
-            </Card>
-          </div>
+                    <div data-vibe='card'>
+                        <Card>
+                            <CardMedia
+                                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                                >
+                                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
+                            </CardMedia>
+                            <CardTitle title="Card title" subtitle="Card subtitle" />
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
+                                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
+                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                            </CardText>
+                            <CardActions>
+                                <FlatButton label="Action1" />
+                                <FlatButton label="Action2" />
+                            </CardActions>
+                        </Card>
+                    </div>
 
-          <div data-vibe='card'>
-            <Card>
-              <CardMedia
-                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
-              </CardMedia>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
-                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-              </CardText>
-              <CardActions>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-              </CardActions>
-            </Card>
-          </div>
+                    <div data-vibe='card'>
+                        <Card>
+                            <CardMedia
+                                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                                >
+                                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
+                            </CardMedia>
+                            <CardTitle title="Card title" subtitle="Card subtitle" />
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
+                                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
+                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                            </CardText>
+                            <CardActions>
+                                <FlatButton label="Action1" />
+                                <FlatButton label="Action2" />
+                            </CardActions>
+                        </Card>
+                    </div>
 
-          <div data-vibe='card'>
-            <Card>
-              <CardMedia
-                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
-              </CardMedia>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
-                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-              </CardText>
-              <CardActions>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-              </CardActions>
-            </Card>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                    <div data-vibe='card'>
+                        <Card>
+                            <CardMedia
+                                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                                >
+                                <img src='http://previews.123rf.com/images/rastudio/rastudio1505/rastudio150500060/39497815-Musical-note-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-Vector-dark-grey-icon-Stock-Vector.jpg'/>
+                            </CardMedia>
+                            <CardTitle title="Card title" subtitle="Card subtitle" />
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec mattis pretium massa.Aliquam erat volutpat.Nulla facilisi.
+                                Donec vulputate interdum sollicitudin.Nunc lacinia auctor quam sed pellentesque.
+                                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                            </CardText>
+                            <CardActions>
+                                <FlatButton label="Action1" />
+                                <FlatButton label="Action2" />
+                            </CardActions>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
